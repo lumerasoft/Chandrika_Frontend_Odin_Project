adb28cd814e51c9c1fe2c0e603e03d4092642373
Chandrikagedala <chandrika@lumerasoft.com>
2023-02-09 11:39:10 +0530

    add routes

--- online-order-frontend/src/main.js
+++ online-order-frontend/src/main.js
@@ -4,17 +4,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-// import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
-// import 'bootstrap-vue/dist/bootstrap-vue.css';
-// import 'bootstrap/dist/css/bootstrap.css';
 import CreateProduct from './components/CreateProduct.vue'
 import ListProducts from './components/ListProducts.vue'
 
-// Vue.use(BootstrapVue);
-// Vue.config.productionTip = false
 const routes = [
   {
-    path: '/new_product',
+    path: '/',
+    component: ListProducts,
+  },
+  {
+    path: '/create_product',
     component: CreateProduct,
   },
   {
