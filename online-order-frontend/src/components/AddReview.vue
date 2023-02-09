5ca754a2e04ec0df1f73acbc7fe4cb4767794d1c
Chandrikagedala <chandrika@lumerasoft.com>
2023-02-09 18:16:16 +0530

    adding reviews

---
+++ online-order-frontend/src/components/AddReview.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mb-3">
+    <form>
+      <label class="form-label"><b>Review</b></label>
+      <input type="text" id="review" class="form-control" v-model="review">
+      <button v-on:click="create" type="submit">Create</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "AddReview",
+
+    data() {
+      return {
+        review: '',
+        reviews: []
+      }
+    },
+
+    methods: {
+      create: function() {
+        const existing_reviews = localStorage.getItem("reviews")
+        let parsed_data = JSON.parse(existing_reviews)
+        let new_review = {review: this.review}
+        parsed_data.push(new_review)
+        localStorage.setItem("reviews",JSON.stringify(parsed_data))
+
+        this.review = ""
+      }
+    },
+  }
+</script>
