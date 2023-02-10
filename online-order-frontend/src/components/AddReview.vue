37f4bf2b43a842f61abc36eb320920eee374a845
Chandrikagedala <chandrika@lumerasoft.com>
2023-02-10 11:32:07 +0530

    add and display review

--- online-order-frontend/src/components/AddReview.vue
+++ online-order-frontend/src/components/AddReview.vue
@@ -1,11 +1,20 @@
 <template>
+<h1>Add Review</h1>
   <div class="mb-3">
     <form>
       <label class="form-label"><b>Review</b></label>
       <input type="text" id="review" class="form-control" v-model="review">
-      <button v-on:click="create" type="submit">Create</button>
+      <button v-on:click="create" type="button">Create</button>
     </form>
   </div>
+  <p>Reviews: {{this.count}}</p>
+    <!-- <p class="card-text">{{this.review}}</p> -->
+    <!-- <p v-for="review in reviews" :key="review"> </p> -->
+      <li class="card-text" v-for="review in reviews" :key="review">{{review.review}}</li>
+    <!-- </p> -->
+
+<!-- <p class="card-text" v-for="review in reviews" :key="review">{{review}}</p> -->
+<!-- <li v-for="review in displayReview" :key="review">{{review}}</li> -->
 </template>
 
 <script>
@@ -15,20 +24,40 @@
     data() {
       return {
         review: '',
-        reviews: []
+        reviews: [],
+        count: ''
       }
     },
 
     methods: {
-      create: function() {
-        const existing_reviews = localStorage.getItem("reviews")
-        let parsed_data = JSON.parse(existing_reviews)
+      // displayReview() {
+      //   this.products = JSON.parse(localStorage.getItem("reviews"))
+      // },
+
+      create() {
+        console.log("hii")
+        let existing_reviews = JSON.parse(localStorage.getItem("reviews"))
+        // let parsed_data = JSON.parse(existing_reviews)
         let new_review = {review: this.review}
-        parsed_data.push(new_review)
-        localStorage.setItem("reviews",JSON.stringify(parsed_data))
+        existing_reviews.push(new_review)
+        localStorage.setItem("reviews",JSON.stringify(existing_reviews))
+        console.log(existing_reviews)
 
         this.review = ""
+      },
+      getCount(){
+        this.count = JSON.parse(localStorage.getItem("reviews")).length
+      },
+      displayReview() {
+        this.reviews = JSON.parse(localStorage.getItem("reviews"))
+        // this.review = JSON.parse(localStorage.getItem("reviews"))
+        // return this.review
+
       }
     },
+    mounted() {
+      this.getCount();
+      this.displayReview();
+    }
   }
 </script>
